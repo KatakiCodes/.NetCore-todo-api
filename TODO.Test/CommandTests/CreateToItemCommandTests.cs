536d5e15b037eee9ea441379b736d5b11d92c3ca
Nelson Dos Santos <katakicode001@gmail.com>
2025-06-17 12:02:50 -0300

    feat: Adicionando testes para os comandos (red, green, refactor)

---
+++ TODO.Test/CommandTests/CreateToItemCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using TODO.Domain.Commands;
+
+namespace TODO.Test.CommandTests;
+
+[TestClass]
+public sealed class CreateTodoItemCommandTests
+{
+    [TestMethod]
+    public void Dado_um_comando_invalido()
+    {
+        var command = new CreateTodoItemCommand("", "", DateTime.Now);
+        command.Validate();
+        Assert.AreEqual(false, command.Valid);
+    }
+    [TestMethod]
+    public void Dado_um_comando_valido()
+    {
+        var command = new CreateTodoItemCommand("Tarefa 1", "andrebaltieri", DateTime.Now);
+        command.Validate();
+        Assert.AreEqual(true, command.Valid);
+    }
+}
