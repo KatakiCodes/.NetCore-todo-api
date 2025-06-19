9751b23f7bf2b5c4cd21baf075d8cb365a5235b7
Nelson Dos Santos <katakicode001@gmail.com>
2025-06-19 06:20:53 -0300

    feat: refatorando os testes dos handlers

--- TODO.Test/HandlersTests/CreateTodoItemHandlerTest.cs
+++ TODO.Test/HandlersTests/CreateTodoItemHandlerTest.cs
@@ -8,16 +8,28 @@ namespace Company.TestProject1;
 [TestClass]
 public class CreateTodoItemHandlerTest
 {
+
+    public CreateTodoItemHandlerTest()
+    {
+        _ValidCommand = new CreateTodoItemCommand("tarefa 1", "andrebaltieri", DateTime.Now);
+        _IvalidCommand = new CreateTodoItemCommand("", "", DateTime.Now);
+        _Handler =  new Handler(new FakeTodoItemRepository());
+    }
+    private readonly CreateTodoItemCommand _IvalidCommand;
+    private readonly CreateTodoItemCommand _ValidCommand;
+    private readonly Handler _Handler;
+
+
     [TestMethod]
     public void Dado_um_comando_invalido_deve_interromper_a_tarefa()
     {
-        var command = new CreateTodoItemCommand("", "", DateTime.Now);
-        var handler = new Handler(new FakeTodoItemRepository());
-        Assert.Fail();
+        var result = (GenericCommandResult) _Handler.Handle(_IvalidCommand);
+        Assert.AreEqual(false, result.Success);
     }
     [TestMethod]
     public void Dado_um_comando_valido_deve_criar_a_tarefa()
     {
-        Assert.Fail();
+        var result = (GenericCommandResult) _Handler.Handle(_ValidCommand);
+        Assert.AreEqual(true, result.Success);
     }
 }
