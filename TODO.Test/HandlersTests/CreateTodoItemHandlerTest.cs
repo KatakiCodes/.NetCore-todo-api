51df77b78ce3a227a4d685ecacc2b97a346502f9
Nelson Dos Santos <katakicode001@gmail.com>
2025-06-19 06:00:16 -0300

    feat: Criando testes para os handlers

---
+++ TODO.Test/HandlersTests/CreateTodoItemHandlerTest.cs
@@ -0,0 +1,20 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TODO.Domain.Commands;
+using TODO.Domain.Handlers;
+
+namespace Company.TestProject1;
+
+[TestClass]
+public class CreateTodoItemHandlerTest
+{
+    [TestMethod]
+    public void Dado_um_comando_invalido_deve_interromper_a_tarefa()
+    {
+        Assert.Fail();
+    }
+    [TestMethod]
+    public void Dado_um_comando_valido_deve_criar_a_tarefa()
+    {
+        Assert.Fail();
+    }
+}
