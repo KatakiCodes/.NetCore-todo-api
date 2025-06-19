990e6d277d2a8adae9819d03b11ce2535d873f5e
Nelson Dos Santos <katakicode001@gmail.com>
2025-06-19 07:24:20 -0300

    feat: Adicionando Dbset Todo

--- TODO.Infra/DataContexts/DataContext.cs
+++ TODO.Infra/DataContexts/DataContext.cs
@@ -1,12 +1,13 @@
 using System;
 using Microsoft.EntityFrameworkCore;
+using TODO.Domain.Entities;
 
 namespace TODO.Infra.DataContexts;
 
 public class DataContext : DbContext
 {
-    public DataContext(DbContextOptions<DataContext>options): base(options)
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        
     }
+    public DbSet<TodoItem> Todos { get; set; }
 }
