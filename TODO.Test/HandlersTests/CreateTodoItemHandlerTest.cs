dcf362fc5851f9ccf469c49fc697089c3fbb9764
Nelson Dos Santos <katakicode001@gmail.com>
2025-06-19 06:04:53 -0300

    feat: Resolvendo a dependência do handler com fake repository no arquivo de teste dos handlers

--- TODO.Test/HandlersTests/CreateTodoItemHandlerTest.cs
+++ TODO.Test/HandlersTests/CreateTodoItemHandlerTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TODO.Domain.Commands;
 using TODO.Domain.Handlers;
+using TODO.Test.Repositories;
 
 namespace Company.TestProject1;
 
@@ -10,6 +11,8 @@ public class CreateTodoItemHandlerTest
     [TestMethod]
     public void Dado_um_comando_invalido_deve_interromper_a_tarefa()
     {
+        var command = new CreateTodoItemCommand("", "", DateTime.Now);
+        var handler = new Handler(new FakeTodoItemRepository());
         Assert.Fail();
     }
     [TestMethod]
---
+++ TODO.Test/Repositories/FakeTodoItemRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using TODO.Domain.Entities;
+using TODO.Domain.Repositories;
+
+namespace TODO.Test.Repositories;
+
+public class FakeTodoItemRepository : ITodoItemRepository
+{
+    public void Create(TodoItem todoItem)
+    {
+    }
+
+    public void Update(TodoItem todoItem)
+    {
+    }
+}
