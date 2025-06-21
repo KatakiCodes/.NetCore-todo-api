55d85c617fe36fff3bbc533b04a9a01f14849bfd
katakicodes <katakicode001@gmail.com>
2025-06-21 14:27:59 -0300

    fix: Implementando a interface nos fakesrepo

--- TODO.Test/Repositories/FakeTodoItemRepository.cs
+++ TODO.Test/Repositories/FakeTodoItemRepository.cs
@@ -19,4 +19,29 @@ public class FakeTodoItemRepository : ITodoItemRepository
     public void Update(TodoItem todoItem)
     {
     }
+
+    public IEnumerable<TodoItem> GetAll(string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TodoItem GetById(string user, Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TodoItem> GetAllDone(string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TodoItem> GetAllUndone(string user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done)
+    {
+        throw new NotImplementedException();
+    }
 }
