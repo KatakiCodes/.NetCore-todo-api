c1f38b7e6ae9dd8f99ffae4fe535a6900eca6a03
katakicodes <katakicode001@gmail.com>
2025-06-21 13:51:28 -0300

    feat: Resolvendo dependencia do repositorio

--- TODO.Api/Program.cs
+++ TODO.Api/Program.cs
@@ -1,13 +1,21 @@
 using Microsoft.EntityFrameworkCore;
+using TODO.Domain.Handlers;
+using TODO.Domain.Repositories;
 using TODO.Infra.DataContexts;
+using TODO.Infra.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-//Add dbcontext
+
+//Soving dependencies
 builder.Services.AddDbContext<DataContext>(option => option.UseInMemoryDatabase("Database"));
+
+builder.Services.AddTransient<ITodoItemRepository, TodoRepository>();
+builder.Services.AddTransient<Handler, Handler>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
