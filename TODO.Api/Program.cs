6dfbf2e2dd613622c92efe526d2b63d6a78e4645
katakicodes <katakicode001@gmail.com>
2025-06-23 21:24:03 -0300

    feat: adicionando swagger

--- TODO.Api/Program.cs
+++ TODO.Api/Program.cs
@@ -9,21 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddControllers();
 //Soving dependencies
 builder.Services.AddDbContext<DataContext>(option => option.UseInMemoryDatabase("Database"));
 
 builder.Services.AddTransient<ITodoItemRepository, TodoRepository>();
 builder.Services.AddTransient<Handler, Handler>();
-
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.UseHttpsRedirection();
 
 app.Run();
